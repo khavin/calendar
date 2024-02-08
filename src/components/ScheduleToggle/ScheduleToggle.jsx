7a46317d9c53390ed8797c1b2c4fc824e2f243ee
Khavin K K <khavinkrishnan@gmail.com>
2024-02-07 19:55:39 -0500

    added padding related styles for schedule toggles

--- src/components/ScheduleToggle/ScheduleToggle.jsx
+++ src/components/ScheduleToggle/ScheduleToggle.jsx
@@ -3,9 +3,9 @@ import styles from "./index.module.scss";
 export default function ScheduleToggle() {
   return (
     <div className={styles.container}>
-      <span className="selected frost_button">Day</span>
-      <span className="frost_button">Week</span>
-      <span className="frost_button">Month</span>
+      <span className={`${styles.selected} ${styles.toggle}`}>Day</span>
+      <span className={styles.toggle}>Week</span>
+      <span className={styles.toggle}>Month</span>
     </div>
   );
 }
