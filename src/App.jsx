033068ed7563355bdd3f3c20dc002bd04126c7ba
Khavin K K <khavinkrishnan@gmail.com>
2024-02-16 21:04:34 -0500

    added context for detecting mobile screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+
 import "./App.css";
 import Header from "./components/Header";
 import SidePanel from "./components/SidePanel";
 import MainPanel from "./components/MainPanel";
 
+export const DeviceContext = createContext(null);
+
 function App() {
+  const mq = window.matchMedia("(max-width: 650px)");
+  const [isMobile, setIsMobile] = useState(mq.matches);
+
+  useEffect(() => {
+    mq.addEventListener("change", (e) => {
+      setIsMobile(e.matches);
+    });
+  }, []);
+
   return (
     <div>
-      <Header />
-      <div className="content-panel">
-        <SidePanel />
-        <MainPanel />
-      </div>
+      <DeviceContext.Provider value={isMobile}>
+        <Header />
+        <div className="content-panel">
+          <SidePanel />
+          <MainPanel />
+        </div>
+      </DeviceContext.Provider>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
+import { useContext, useEffect } from "react";
+import { DeviceContext } from "../../App";
 import "./index.scss";
+import hamburgerSrc from "/src/assets/hamburger_icon.png";
 
 export default function Header() {
+  const isMobile = useContext(DeviceContext);
+
+  useEffect(() => {
+    console.log(isMobile);
+  }, [isMobile]);
+
   return (
     <div className="header">
-      <div className="app-name">Calendar</div>
+      <div className="app-and-menu-container">
+        <span>
+          <img src={hamburgerSrc}></img>&nbsp;&nbsp;
+        </span>
+
+        <div className="app-name">Calendar</div>
+      </div>
+
       {/* TODO - add right side panel */}
       <div className="header-right-panel"></div>
     </div>
